3fe3cd1d502012553bdf123446314d02bcdb96c4
Mark <markmeldrum70@gmail.com>
2020-12-05 12:01:30 +0000

    RegionSelect

--- client/src/components/FoodSelect.vue
+++ client/src/components/FoodSelect.vue
@@ -1,10 +1,17 @@
 <template>
-  
+    <li v-on:click="handleClick">{{region.name}}</li>
 </template>
 
 <script>
+import { eventBus } from '../main.js'
 export default {
-
+    name: 'region-select',
+    props: ['region'],
+    methods: {
+        handleClick(){
+            eventBus.$emit('region-selected', this.region)
+        }
+    }
 }
 </script>
 
