ba0f96337bdc74ba36322cd321a27f8add687b1a
Mark <markmeldrum70@gmail.com>
2020-12-05 12:20:34 +0000

    RegionDetail

--- client/src/components/FoodDetail.vue
+++ client/src/components/FoodDetail.vue
@@ -1,9 +1,23 @@
 <template>
-  
+    <div v-if='food' id="foodDetail">
+        <div>
+            <h2>{{food.name}}</h2>
+        </div>
+        <div>
+            <p><span>Name</span>: {{food.name}}</p>
+            <p><span>Description</span>: {{food.description}}</p>
+            <p><span>Key Ingredients</span>: {{food.ingredients}}</p>
+            <p><span>Origin</span>: {{food.origin}}</p>
+            <p><span>Region</span>: {{food.region}}</p>
+        </div>
+        <!-- URL's ?? -->
+    </div>
 </template>
 
 <script>
 export default {
+    name: 'food-detail',
+    
 
 }
 </script>
