e0ceba0e7ac84ecc8904a6fab00f0c99c72eab52
Mark <markmeldrum70@gmail.com>
2020-12-05 11:19:25 +0000

    Client files setup

---
+++ client/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  
+</template>
+
+<script>
+
+import RegionList from "./components/RegionList.vue" 
+import RegionSelect from "./components/RegionSelect.vue" 
+import RegionDetail from "./components/RegionDetail.vue" 
+import FoodList from "./components/FoodList.vue" 
+import FoodDetail from "./components/FoodDetail.vue" 
+import FoodSelect from "./components/FoodSelect.vue" 
+import { eventBus } from './main.js';
+
+export default {
+    name: "app",
+    data() {
+        return{
+            foods: [],
+            selectedFood: null,
+            regions: [],
+            selectedRegion: null
+        }
+    },
+    components: {
+        "region-list": RegionList,
+        "region-select": RegionSelect,
+        "region-detail": RegionDetail,
+        "food-list": FoodList,
+        "food-detail": FoodDetail,
+        "food-select": FoodSelect
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
