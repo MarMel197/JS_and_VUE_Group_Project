31f4b1dd3c1d5f03e5504c4aa9433d8641271bf9
Mark <markmeldrum70@gmail.com>
2020-12-07 16:40:29 +0000

    Changed regions in DB

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,7 +7,7 @@ db.menus.insertMany([
     description: "A common English side dish, a baked pudding made from a batter of eggs, flour, and milk or water.",
     key_ingredients: "Eggs, Flour, and Milk or Water",
     origin: "Originated in northern England",
-    region: "North",
+    region: "England",
     image_url: "https://media.gettyimages.com/photos/freshly-baked-yorkshire-puddings-picture-id172752978?k=6&m=172752978&s=612x612&w=0&h=zPoKjQi8wTbp8QL-HpeTET0NT6e95hCh2BCoS82KIbU=",
     map_url: "cccc"
     },
@@ -34,7 +34,7 @@ db.menus.insertMany([
     description: "A a quick bread with added sultanas and raisins..",
     key_ingredients: "beef skirt, potato, swede (turnip) and onion",
     origin: "Originated in in South West England.",
-    region: "Cornwall",
+    region: "England",
     image_url: "https://www.thespruceeats.com/thmb/VePbSFe-byPmYO63146dq64OT0M=/2932x1955/filters:fill(auto,1)/82738399-58adae853df78c345bb55cce.jpg",
     map_url: "ffff" 
     },
@@ -43,7 +43,7 @@ db.menus.insertMany([
     description: "Chopped eels boiled in a spiced stock that is allowed to cool and set, forming a jelly.",
     key_ingredients: "Eel and spiced stock",
     origin: "Originated in the 18th century, primarily in the East End of London..",
-    region: "London",
+    region: "England",
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Eels_1385.JPG/1200px-Eels_1385.JPG",
     map_url: "hhhh" 
     },
@@ -52,7 +52,7 @@ db.menus.insertMany([
     description: "Crabs with chalk shelf and found in nutrient-rich waters. Tender, fragrant and well-meated with high proportion of white to brown meat.",
     key_ingredients: "well-salted water, seasoning, vegetables",
     origin: "Northern coast of Norfolk ",
-    region: "Cromer Village",
+    region: "England",
     image_url: "https://www.slowfood.org.uk/wp-content/uploads/2014/05/brownCrab-Pengellys-with-permission.jpg",
     map_url: "hhhh" 
     }
